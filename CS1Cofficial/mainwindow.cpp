559b222878669c2d7a86099dd78a4c6e7f6e7c5a
Alexis Luna <calixluna4@gmail.com>
2018-11-20 20:09:31 -0800

    added contact form

--- CS1Cofficial/mainwindow.cpp
+++ CS1Cofficial/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "contact.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    contact *c = new contact();
+    c->show();
 }
 
 MainWindow::~MainWindow()
