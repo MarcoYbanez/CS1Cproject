25f6533a138681058f17c9ee57daaa9012a825b6
Imaad Mohammed <imaadmohammed@gmail.com>
2018-12-07 22:53:09 -0800

    Update listing.h

--- CS1Cofficial/listing.h
+++ CS1Cofficial/listing.h
@@ -2,98 +2,110 @@
 #define LISTING_H
 #include "shape.h"
 #include "vector.h"
-#include <fstream>
+#include "data.h"
+#include "circle.h"
+#include "ellipse.h"
+#include "line.h"
+#include "polygon.h"
+#include "polyline.h"
+#include "rectangle.h"
+#include "square.h"
+#include "text.h"
 class listing
 {
 private:
 
 public:
-   void idList();
-   void areaList();
-   void periList();
-   Shape * currentShape;
-   void swap(int *xp, int *yp);
-   void swapDouble(double*xp, double*yp);
-   void bubbleSort(int arr[], int n);
-   void bubbleSortDouble(double arr[], int n);
-};
+    int idsP[6];
+    string typeP[6];
+    int idsA[6];
+    string typeA[6];
+    double areasA[6];
+    double perimetersA[6];
+    double areasP[6];
+    double perimetersP[6];
 
-void idList()
-{
-    vectorType<Shape> shapeV;
-   std::ifstream shapeFile;
-   int ids[8];
-   shapeFile.open("/Users/imaad/Documents/CS1Cproject/CS1Cofficial/shapes.txt");
-   // shapeV.parse();
-   // for (int i = 0; i < shapeV.getSize(); i++)
-     //   shapeV[i].getId() = ids[i];
-
-}
-
-void areaList()
-{
-    vectorType<Shape> shapeV;
-   std::ifstream shapeFile;
-   double areas[6];
-   shapeFile.open("/Users/imaad/Documents/CS1Cproject/CS1Cofficial/shapes.txt");
-   //shapeV.parse();
-   for (int i = 0; i < shapeV.getSize(); i++)
-   {
-     //  if (shapeV[i].getArea() > 0)
-       //    shapeV[i].getArea() = areas[i];
-   }
-}
-
-void periList()
-{
-    vectorType<Shape> shapeV;
-    std::ifstream shapeFile;
-    double perimeters[6];
-    shapeFile.open("/Users/imaad/Documents/CS1Cproject/CS1Cofficial/shapes.txt");
-    //shapeV.parse();
-    for (int i = 0; i < shapeV.getSize(); i++)
+    void idList()
     {
-     //   if (shapeV[i].getPerimeter() > 0)
-           // shapeV[i].getPerimeter() = perimeters[i];
+       int ids[8];
+       string type[8];
+       int x[8];
+       int y[8];
+       string color[8];
+       int width[8];
+       string penStyle[8];
+        for (int i = 0; i < data::shapeVector.getSize(); i++)
+           ids[i] = data::shapeVector[i]->getShapeId();
+        bubbleSort(ids,8);
     }
 
-}
+    void areaList()
+    {
 
-void swap(int *xp, int *yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+       for (int i = 0; i < data::shapeVector.getSize(); i++)
+           if (data::shapeVector[i]->getArea() > 0)
+                areasA[i] = data::shapeVector[i]->getArea();
+        bubbleSortDouble(areasA,6);
+        for (int i = 0; i < data::shapeVector.getSize(); i++)
+            if (areasA[i] ==  data::shapeVector[i]->getArea())
+            {
+                typeA[i] = data::shapeVector[i]->getTextString();
+                idsA[i] = data::shapeVector[i]->getShapeId();
+                perimetersA[i] = data::shapeVector[i]->getPerimeter();
+            }
+    }
 
-void swapDouble (double *xp, double *yp)
-{
-    double temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
+    void periList()
+    {
 
-// A function to implement bubble sort
-void bubbleSort(int arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)
+        for (int i = 0; i < data::shapeVector.getSize(); i++)
+            if (data::shapeVector[i]->getPerimeter() > 0)
+                perimetersP[i] = data::shapeVector[i]->getPerimeter();
+        bubbleSortDouble(perimetersP, 6);
+        for (int i = 0; i < data::shapeVector.getSize(); i++)
+            if (perimetersP[i] ==  data::shapeVector[i]->getPerimeter())
+            {
+                typeP[i] = data::shapeVector[i]->getTextString();
+                idsP[i] = data::shapeVector[i]->getShapeId();
+                areasA[i] = data::shapeVector[i]->getArea();
+            }
+    }
 
-       // Last i elements are already in place
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
-}
+   void swap(int *xp, int *yp)
+   {
+       int temp = *xp;
+       *xp = *yp;
+       *yp = temp;
+   }
 
+   void swapDouble (double *xp, double *yp)
+   {
+       double temp = *xp;
+       *xp = *yp;
+       *yp = temp;
+   }
 
-void bubbleSortDouble(double arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)
-       // Last i elements are already in place
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
-              swapDouble(&arr[j], &arr[j+1]);
-}
+   // A function to implement bubble sort
+   void bubbleSort(int arr[], int n)
+   {
+      int i, j;
+      for (i = 0; i < n-1; i++)
+          // Last i elements are already in place
+          for (j = 0; j < n-i-1; j++)
+              if (arr[j] > arr[j+1])
+                 swap(&arr[j], &arr[j+1]);
+   }
 
-#endif // LISTING_H
+   void bubbleSortDouble(double arr[], int n)
+   {
+      int i, j;
+      for (i = 0; i < n-1; i++)
+          // Last i elements are already in place
+          for (j = 0; j < n-i-1; j++)
+              if (arr[j] > arr[j+1])
+                 swapDouble(&arr[j], &arr[j+1]);
+   }
+
+};
+
+#endif //LISTING_H
