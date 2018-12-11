a887d4851be365bb5cdff07db0ba76d19c2a0811
MarcoYbanez <marcowrk19@gmail.com>
2018-12-10 23:30:19 -0800

    Added files from Imaad pull request

--- CS1Cofficial/circle.h
+++ CS1Cofficial/circle.h
@@ -12,35 +12,35 @@ class Circle : public Shape
 private:
     int x;
     int y; //(x1,y1) top left point
-    int a; //semi-major axis
-    int b; //semi-minor axis
-    int area;
-    int p;
+    int ax; //semi-major axis
+    int bx; //semi-minor axis
+    double a;
+    double p;
 public:
      Circle(): Shape()
     {
-      a = 0;
-      b = 0;
+      ax = 0;
+      bx = 0;
     }
 
     Circle(int a1, int b1): Shape()
     {
-      a = a1;
-      b = b1;
+      ax = a1;
+      bx = b1;
     }
 
     Circle(const Circle& other): Shape()
     {
-      this->a = other.a;
-      this->b = other.b;
+      this->ax = other.ax;
+      this->bx = other.bx;
     }
 
     void setCord(int a1,int b1,int a2,int b2)
     {
         x = a1;
         y = b1;
-        a = a2;
-        b = b2;
+        ax = a2;
+        bx = b2;
         }
 
     void draw(QPainter &p)
@@ -48,7 +48,7 @@ public:
         p.setPen(getPen());
         p.setBrush(getBrush());
 
-        p.drawEllipse(QPointF(x,y),a, a );
+        p.drawEllipse(QPointF(x,y),ax, ax );
     }
 
     void move()
@@ -59,19 +59,39 @@ public:
     double getPerimeter()
     {
       double h=0;
-      double base1 = (a-b), base2 = (a+b), power = 2;
+      double base1 = (ax-bx), base2 = (ax+bx), power = 2;
       h = pow(base1, power)/pow(base2, power);
       double square = sqrt(4-3*h);
       p = PI*base2*(1+3*h/10+square);
-   return p;
+      return p;
     }
 
     double getArea()
     {
-      area = PI * a * b;
-    return area;
-
+      a = PI * ax * bx;
+    return a;
     }
+
+    int getX()
+    {
+        return x;
+    }
+
+    int getY()
+    {
+        return y;
+    }
+
+    int getAx()
+    {
+        return ax;
+    }
+
+    int getBx()
+    {
+        return bx;
+    }
+
 };
 
 #endif // CIRCLE_H
